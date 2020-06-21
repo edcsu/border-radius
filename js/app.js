3201cf0a7799d85f9e8d4f79a071e851f2bae6e0
Keith Ssewanonda <keith@laboremus.no>
2020-06-21 16:09:18 +0300

    feat: show border radius styles

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 $(() => {
     $("#topLeft").on('input', function(){
         let tl = `${$("#topLeft").val()}px`;
-        $(".br-top-left").css({'border-top-left-radius' : tl});
+        $(".br-style").css({'border-top-left-radius' : tl});
         let value = $("#style-me").css("border-top-left-radius");
+        $("#btl-value").text(`${value}`);
     });
     $("#topRight").on('input', () =>{
         let tr = `${$("#topRight").val()}px`;
-        $(".br-top-right").css({'border-top-right-radius' : tr});
-        let value = $("#style-me").css("border-top-left-radius");
+        $(".br-style").css({'border-top-right-radius' : tr});
+        let value = $("#style-me").css("border-top-right-radius");
+        $("#btr-value").text(`${value}`);
     });
     $("#bottomRight").on('input', () =>{
         let br = `${$("#bottomRight").val()}px`;
-        $(".br-bottom-right").css({'border-bottom-right-radius' : br});
-        let value = $("#style-me").css("border-top-left-radius");
+        $(".br-style").css({'border-bottom-right-radius' : br});
+        let value = $("#style-me").css("border-bottom-right-radius");
+        $("#bbr-value").text(`${value}`);
     });
     $("#bottomLeft").on('input', () =>{
         let bl = `${$("#bottomLeft").val()}px`;
-        $(".br-bottom-left").css({'border-bottom-left-radius' : bl});
-        let value = $("#style-me").css("border-top-left-radius");
+        $(".br-style").css({'border-bottom-left-radius' : bl});
+        let value = $("#style-me").css("border-bottom-left-radius");
+        $("#bbl-value").text(`${value}`);
     });
 });
