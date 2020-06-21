d28ec42f539ecf754fd6db87a4efcf3f2ea24dc0
Keith Ssewanonda <keith@laboremus.no>
2020-06-21 17:20:27 +0300

    refactor:  set border radius function

--- js/app.js
+++ js/app.js
@@ -1,38 +1,31 @@
 $(() => {
-    $("#topLeft").on('input', function(){
-        let tl = `${$("#topLeft").val()}px`;
-        $(".br-style").css({'border-top-left-radius' : tl});
-        let value = $("#style-me").css("border-top-left-radius");
-        $("#btl-value").text(`${value}`);
-    });
-    $("#topRight").on('input', () =>{
-        let tr = `${$("#topRight").val()}px`;
-        $(".br-style").css({'border-top-right-radius' : tr});
-        let value = $("#style-me").css("border-top-right-radius");
-        $("#btr-value").text(`${value}`);
-    });
-    $("#bottomRight").on('input', () =>{
-        let br = `${$("#bottomRight").val()}px`;
-        $(".br-style").css({'border-bottom-right-radius' : br});
-        let value = $("#style-me").css("border-bottom-right-radius");
-        $("#bbr-value").text(`${value}`);
-    });
-    $("#bottomLeft").on('input', () =>{
-        let bl = `${$("#bottomLeft").val()}px`;
-        $(".br-style").css({'border-bottom-left-radius' : bl});
-        let value = $("#style-me").css("border-bottom-left-radius");
-        $("#bbl-value").text(`${value}`);
-    });
-    
+    // set for top left radius
+    setBorderRadius("#topLeft", ".br-style", "#style-me", "border-top-left-radius", "#btl-value");
+    // set for top right radius
+    setBorderRadius("#topRight", ".br-style", "#style-me", "border-top-right-radius", "#btr-value");
+    // set for bottom right radius
+    setBorderRadius("#bottomRight", ".br-style", "#style-me", "border-bottom-right-radius", "#bbr-value");
+    // set for bottom left radius
+    setBorderRadius("#bottomLeft", ".br-style", "#style-me", "border-bottom-left-radius", "#bbl-value");
+
     $("#copy-text").click(() =>{
         copyToClipboard("#br-details");
     });
 });
 
-function copyToClipboard(element) {
-    var $temp = $("<input>");
+setBorderRadius = (selector, styleClassName, styledDiv, cssProperty, textOuptut) => {
+    $(selector).on('input', () =>{
+        let inputValue = `${$(selector).val()}px`;
+        $(styleClassName).css(`${cssProperty}`, inputValue);
+        let value = $(styledDiv).css(`${cssProperty}`);
+        $(textOuptut).text(`${value}`);
+    });
+}
+
+copyToClipboard = (element) => {
+    let $temp = $("<input>");
     $("body").append($temp);
     $temp.val($(element).text()).select();
     document.execCommand("copy");
     $temp.remove();
-  }
+}
