41bf9820585437ed2b53438027c39883d9e4d70b
Keith Ssewanonda <keith@laboremus.no>
2020-06-21 14:19:00 +0300

    feat: add set border radous functionality

--- js/app.js
+++ js/app.js
@@ -1,14 +1,22 @@
 $(() => {
-    $("#topLeft").on('input', () =>{
-        console.log($("#topLeft").val());
+    $("#topLeft").on('input', function(){
+        let tl = `${$("#topLeft").val()}px`;
+        $(".br-top-left").css({'border-top-left-radius' : tl});
+        let value = $("#style-me").css("border-top-left-radius");
     });
     $("#topRight").on('input', () =>{
-        console.log($("#topRight").val());
+        let tr = `${$("#topRight").val()}px`;
+        $(".br-top-right").css({'border-top-right-radius' : tr});
+        let value = $("#style-me").css("border-top-left-radius");
     });
     $("#bottomRight").on('input', () =>{
-        console.log($("#bottomRight").val());
+        let br = `${$("#bottomRight").val()}px`;
+        $(".br-bottom-right").css({'border-bottom-right-radius' : br});
+        let value = $("#style-me").css("border-top-left-radius");
     });
     $("#bottomLeft").on('input', () =>{
-        console.log($("#bottomLeft").val());
+        let bl = `${$("#bottomLeft").val()}px`;
+        $(".br-bottom-left").css({'border-bottom-left-radius' : bl});
+        let value = $("#style-me").css("border-top-left-radius");
     });
 });
